85f5870ff258abcf64b2226873b048f52608043a
RAREBEEF <drrobot409@gmail.com>
2022-04-01 16:35:42 +0900

    Add console log

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import "./index.css";
 import App from "./components/App";
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import { fireBase } from "./fbase";
 
 declare global {
   interface Window {
@@ -12,7 +11,17 @@ declare global {
   }
 }
 
-console.log(fireBase);
+console.log(`
+#####      ##     #####    ######   #####    ######   ######   ######  
+##  ##    ####    ##  ##   ##       ##  ##   ##       ##       ##      
+##  ##   ##  ##   ##  ##   ##       ##  ##   ##       ##       ##      
+#####    ######   #####    ####     #####    ####     ####     ####    
+####     ##  ##   ####     ##       ##  ##   ##       ##       ##      
+## ##    ##  ##   ## ##    ##       ##  ##   ##       ##       ##      
+##  ##   ##  ##   ##  ##   ######   #####    ######   ######   ##     
+
+drrobot409@gmail.com\n\nhttps://github.com/RAREBEEF\n\nhttps://velog.io/@drrobot409
+`);
 
 ReactDOM.render(
   <React.StrictMode>
