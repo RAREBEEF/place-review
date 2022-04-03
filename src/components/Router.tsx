fa1ff6e69068f2239808fe41794a6aee3eebdd06
RAREBEEF <drrobot409@gmail.com>
2022-04-03 16:42:29 +0900

    Mobile Footer

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,7 +1,6 @@
 import { ReactElement } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
-import Loading from "../pages/Loading";
 import Profile from "../pages/Profile";
 import { RouterPropType } from "../types";
 import Nav from "./Nav";
